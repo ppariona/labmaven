acfa5fbc173eebd3f0f63cfb14f579766cb3f391
manuelzegarra <mzegarra@gmail.com>
2021-10-16 15:32:04 -0500

    fast commit

---
+++ src/test/java/cloud/csonic/labmaven/LabmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.csonic.labmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
