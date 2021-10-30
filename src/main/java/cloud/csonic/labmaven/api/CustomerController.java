f03e19c95899fe3b4f69eb2a8af299636d207f4a
manuelzegarra <mzegarra@gmail.com>
2021-10-29 20:43:42 -0500

    add JaCoCo

--- src/main/java/cloud/csonic/labmaven/api/CustomerController.java
+++ src/main/java/cloud/csonic/labmaven/api/CustomerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/apis/customers")
 @AllArgsConstructor
 public class CustomerController {
 
