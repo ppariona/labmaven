16ec7481527e00581630e14819ef810541389cdc
manuelzegarra <mzegarra@gmail.com>
2021-11-01 06:54:36 -0500

    add JaCoCo

--- src/test/java/cloud/csonic/labmaven/api/CustomerControllerTest.java
+++ src/test/java/cloud/csonic/labmaven/api/CustomerControllerTest.java
@@ -58,7 +58,7 @@ class CustomerControllerTest {
 
         when(customerService.listAll()).thenReturn(list);
 
-        this.mockMvc.perform(get("/customers_2"))
+        this.mockMvc.perform(get("/customers"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.customers", hasSize(2)))
