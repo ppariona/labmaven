7b6d32e888d58e2d307e289ec066eca739f6cb6e
manuelzegarra <mzegarra@gmail.com>
2021-11-01 07:59:51 -0500

    add JaCoCo

--- src/main/java/cloud/csonic/labmaven/api/CustomerController.java
+++ src/main/java/cloud/csonic/labmaven/api/CustomerController.java
@@ -2,15 +2,12 @@ package cloud.csonic.labmaven.api;
 
 import cloud.csonic.labmaven.api.dto.CustomerDto;
 import cloud.csonic.labmaven.api.dto.CustomersDto;
-import cloud.csonic.labmaven.model.Customer;
 import cloud.csonic.labmaven.service.CustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
--- src/main/java/cloud/csonic/labmaven/api/dto/CustomersDto.java
+++ src/main/java/cloud/csonic/labmaven/api/dto/CustomersDto.java
@@ -2,7 +2,6 @@ package cloud.csonic.labmaven.api.dto;
 
 import cloud.csonic.labmaven.model.Customer;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
