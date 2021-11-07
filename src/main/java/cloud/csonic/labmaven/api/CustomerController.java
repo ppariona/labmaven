f98ba38687d4bb4cb3cbe221e78d48a1977befec
manuelzegarra <mzegarra@gmail.com>
2021-11-07 01:03:08 -0500

    fast commit

--- src/main/java/cloud/csonic/labmaven/api/CustomerController.java
+++ src/main/java/cloud/csonic/labmaven/api/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/customers2")
 @AllArgsConstructor
 public class CustomerController {
 
